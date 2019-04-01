47658f827985859bf8df023eac2f12f016afaf02
Jiaqi Zheng <zhengjiaqi.jessie@gmail.com>
2019-04-01 13:21:46 +0200

    fixed calibration bug, whole simulated area horizontal scroll for the correct geo-coordinates

--- sim_output_trans.py
+++ sim_output_trans.py
@@ -30,18 +30,16 @@ def intoGeo(borderA, borderB, borderC, borderD):
             agt[i][1] = float(temp[1])
 
     # turn into geo-coordinates
-    # parameters (41&18) calibration fulfilled: matches (-20.4,-8.7)-(14.6,8.7) rectangle
-    xRate = [(borderC[0] - borderB[0])/41, (borderC[1] - borderB[1])/41]
-    yRate = [(borderB[0] - borderA[0])/18, (borderB[1] - borderA[1])/18]
+    # 35x17.4m is the real dimension of the simulated rectangle with geo calibration
+    xRate = [(borderC[0] - borderB[0])/35, (borderC[1] - borderB[1])/35]
+    yRate = [(borderB[0] - borderA[0])/17.4, (borderB[1] - borderA[1])/17.4]
     geoCenter = [(borderA[0] + borderC[0])/2, (borderA[1] + borderC[1])/2]
     for agt in trans:
         for i in range(len(agt)):
             xGeo = agt[i][0] * xRate[0] + agt[i][1] * yRate[0]
             yGeo = agt[i][0] * xRate[1] + agt[i][1] * yRate[1]
-            agt[i][0] = xGeo + geoCenter[0]
-            agt[i][1] = yGeo + geoCenter[1]
-
-    # turn trans into final deckgl format [{'trajectory':[[x,y,t]]}]
+            agt[i][0] = xGeo + geoCenter[0] - 0.082*(18.064249995157855-18.063598779214896)
+            agt[i][1] = yGeo + geoCenter[1] - 0.082*(59.33535531252224-59.3351910473156)
     res = []
     for agt in trans:
         res.append({'trajectory': agt})
